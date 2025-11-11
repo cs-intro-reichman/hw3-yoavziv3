23f3040a08430652186285d9db8e1750c725b351
Yoav Ziv <yoav.ziv02@post.runi.ac.il>
2025-11-11 18:23:25 +0200

    finish

--- Algebra.java
+++ Algebra.java
@@ -6,7 +6,7 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
+	    System.out.println(plus(0,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
  		System.out.println(times(3,4));  // 3 * 4
@@ -18,50 +18,144 @@ public class Algebra {
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
    		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
+   		System.out.println(sqrt(169));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int count = 0;
+		if (x1 >= 0)
+		{
+			for (int i=0; i<x1; i++)
+			{
+				count++;
+			}
+		}
+		else
+		{
+			for (int i=0; i>x1; i--)
+			{
+				count--;
+			}
+		}
+		if (x2 >= 0)
+		{
+			for (int i=0; i<x2; i++)
+			{
+				count++;
+			}
+		}
+		else
+		{
+			for (int i=0; i>x2; i--)
+			{
+				count--;
+			}
+		}
+		return count;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int count = x1;
+		if (x2 > 0)
+		{
+			for (int i=0; i<x2;i++)
+			{
+				count--;
+			}
+		}
+		else
+		{
+			for (int i=0; i<x2;i++)
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int count = 0;
+		if (x2 > 0)
+		{
+			for (int i=0; i<x2;i++)
+			{
+				count = count + x1;
+			}
+		}
+		else
+		{
+			for (int i=0; i<x2;i++)
+			{
+				count = count - x1;
+			}
+		}
+		return count;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int count = 1;
+		if (n > 0)
+		{
+			for (int i=0; i<n; i++)
+			{
+				count = count * x;
+			}
+		}
+		else if (n == 0)
+		{
+			count = 1;
+		}
+		return count;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int count = 0;
+		while (x1 > 0) 
+		{
+			x1 = x1 - x2;
+			count++;
+		}
+		if (x1 < 0)
+		{
+			count--;
+		}
+		return count;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int count = 0;
+		int newNum = x1;
+		while (newNum > 0) 
+		{
+			newNum = minus(newNum, x2);
+		}
+		if (newNum ==0)
+		{
+			count = 0;
+			return count;
+		}
+		count = newNum + x2;
+		return count;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int closest = 1;
+		for (int i=0; times(i, i) <= x; i++)
+		{
+			if (times(i, i) <= x) 
+			{
+				closest = i;
+			}
+		}
+		return closest;
 	}	  	  
 }
