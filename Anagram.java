4499c6bf85257819eb5ccb2e52a6aa2b46de5254
Yoav Ziv <yoav.ziv02@post.runi.ac.il>
2025-11-11 23:12:34 +0200

    done

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,76 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		str1 = str1.toLowerCase();
+		str2 = str2.toLowerCase();
+		int count1 = 0;
+		int count2 = 0;
+
+		for (int i=0; i<str1.length(); i++)
+		{
+			if (str1.charAt(i) != ' ')
+			{
+				count1++;
+			}
+		}
+		for (int i=0; i<str2.length(); i++)
+		{
+			if (str2.charAt(i) != ' ')
+			{
+				count2++;
+			}
+		}
+		if (count1 != count2)
+		{
+			return false;
+		}
+
+		for (int i=0; i<str1.length(); i++)
+		{
+			int letterCountStr1 = 0;
+			int letterCountStr2 = 0;
+			for (int j=0; j<str1.length(); j++)
+			{
+				if (str1.charAt(i) == str1.charAt(j) && str1.charAt(i) != ' ')
+				{
+					letterCountStr1++;
+				}
+			}
+			for (int p=0; p<str2.length(); p++)
+			{
+				if (str1.charAt(i) == str2.charAt(p) && str1.charAt(i) != ' ')
+				{
+					letterCountStr2++;
+				}
+			}
+			if (letterCountStr1 != letterCountStr2)
+			{
+				return false;
+			}
+		}
+		return true;
 	}
+
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		str = str.toLowerCase();
+		String newWord = "";
+		for (int i=0; i<str.length(); i++)
+		{
+			if (Character.isLetter(str.charAt(i)))
+			{
+				newWord = newWord + str.charAt(i);
+			}
+		}
+		return newWord;
 	} 
-	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String random = new StringBuilder(str).reverse().toString();
+		return random;
 	}
 }
