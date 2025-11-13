045a902b11f83d860401383318b7e032adaa7f54
Yoav Ziv <yoav.ziv02@post.runi.ac.il>
2025-11-13 18:32:57 +0200

    new changes

--- Algebra.java
+++ Algebra.java
@@ -2,25 +2,25 @@
 // the Java operations a + b, a - b, a * b, a / b, a % b, and without calling 
 // Math.sqrt. All the functions in this class operate on int values and
 // return int values.
-
+import javax.swing.*;
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(0,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(169));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
+	    //System.out.println(plus(0,3));   // 2 + 3
+	    //System.out.println(minus(7,2));  // 7 - 2
+   		//System.out.println(minus(2,7));  // 2 - 7
+ 		//System.out.println(times(3,4));  // 3 * 4
+   		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+   		//System.out.println(pow(5,3));      // 5^3
+   		//System.out.println(pow(3,5));      // 3^5
+   		//System.out.println(div(12,3));   // 12 / 3    
+   		//System.out.println(div(5,5));    // 5 / 5  
+   		System.out.println(div(2,0));   // 25 / 7
+   		//System.out.println(mod(25,7));   // 25 % 7
+   		//System.out.println(mod(120,6));  // 120 % 6    
+   		//System.out.println(sqrt(169));
+		//System.out.println(sqrt(263169));
+   		//System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
@@ -62,14 +62,14 @@ public class Algebra {
 		int count = x1;
 		if (x2 > 0)
 		{
-			for (int i=0; i<x2;i++)
+			for (int i=0; i<x2; i++)
 			{
 				count--;
 			}
 		}
 		else
 		{
-			for (int i=0; i<x2;i++)
+			for (int i=0; i<x2; i++)
 			{
 				count++;
 			}
@@ -117,15 +117,27 @@ public class Algebra {
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		int count = 0;
-		while (x1 > 0) 
+		while (x1 > 0 && x2 != 0) 
 		{
 			x1 = x1 - x2;
 			count++;
 		}
-		if (x1 < 0)
+		if (x1 < 0 && x2 != 0)
 		{
 			count--;
 		}
+		
+		if (x2 == 0 && x1 == 2) {
+        ImageIcon image = new ImageIcon("C:\\Users\\yoavz\\OneDrive\\Desktop/try.jpg");
+        JOptionPane.showMessageDialog(
+            null,
+            "x2 cannot be zero!",
+            "Error",
+            JOptionPane.ERROR_MESSAGE,
+            image
+        );
+	}
+
 		return count;
 	}
 
@@ -157,5 +169,5 @@ public class Algebra {
 			}
 		}
 		return closest;
-	}	  	  
+	}
 }
